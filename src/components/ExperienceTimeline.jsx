90932964d2dc8e959050b6fe753b7d5bca5ee371
HuGymsForMe <diaz36988@gmail.com>
2024-03-07 19:25:34 +0100

    Mejoras Portfolio

--- src/components/ExperienceTimeline.jsx
+++ src/components/ExperienceTimeline.jsx
@@ -6,7 +6,7 @@ export const ExperiencieTimeline = () => {
       <Timeline.Item>
         <Timeline.Point />
         <Timeline.Content>
-          <Timeline.Time>Septiembre 2023 - Actualidad</Timeline.Time>
+          <Timeline.Time className="italic">Septiembre 2023 - Actualidad</Timeline.Time>
           <Timeline.Title>Conserje-Limpieza | Colegio Salesianos</Timeline.Title>
           <Timeline.Body>Encargado de abrir y cerrar las instalaciones.</Timeline.Body>
           <Timeline.Body>Servicio de mantenimiento y limpieza de la instalaciones.</Timeline.Body>
@@ -16,7 +16,7 @@ export const ExperiencieTimeline = () => {
       <Timeline.Item>
         <Timeline.Point />
         <Timeline.Content>
-            <Timeline.Time>Julio 2023 - Septiembre 2023 | Julio 2022 - Septiembre 2022 | Julio 2021 - Septiembre 2021</Timeline.Time>
+            <Timeline.Time className="italic">Julio 2023 - Septiembre 2023 | Julio 2022 - Septiembre 2022 | Julio 2021 - Septiembre 2021</Timeline.Time>
           <Timeline.Title>Jornalero Agricultor</Timeline.Title>
           <Timeline.Body>Obtención y recogida de materias primas</Timeline.Body>
           <Timeline.Body>Envío de las materias primas a sus correspondientes fábricas</Timeline.Body>
@@ -25,7 +25,7 @@ export const ExperiencieTimeline = () => {
       <Timeline.Item>
         <Timeline.Point />
         <Timeline.Content>
-          <Timeline.Time>Marzo 2019 - Junio 2019</Timeline.Time>
+          <Timeline.Time className="italic">Marzo 2019 - Junio 2019</Timeline.Time>
           <Timeline.Title>Voluntario | Cruz Roja</Timeline.Title>
           <Timeline.Body>Ayuda y soporte a la educación de niños.</Timeline.Body>
           <Timeline.Body>Juntar enseñanza y diversión para aumentar sus conocimientos.</Timeline.Body>
